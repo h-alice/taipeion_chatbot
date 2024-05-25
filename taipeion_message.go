00009bda5e162b75b785eaa7a41cb2a1ca159fa5
h-alice <admin@halice.art>
2024-05-25 09:32:41 +0800

    Message format.

--- taipeion_message.go
+++ taipeion_message.go
@@ -1,1 +1,15 @@
 package taipeion
+
+import (
+	"fmt"
+)
+
+type Message struct {
+	Type string `json:"type"`
+	Id   string `json:"id"`
+	Text string `json:"text"`
+}
+
+func (m *Message) String() string {
+	return fmt.Sprintf("Message{Type: %s, Id: %s, Text: %s}", m.Type, m.Id, m.Text)
+}
