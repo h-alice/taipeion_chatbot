3a1a9b1114ef2444b8165522a982db70cffbf657
h-alice <admin@halice.art>
2024-05-26 09:57:02 +0800

    Message payload structure

--- taipeion_message.go
+++ taipeion_message.go
@@ -9,6 +9,22 @@ type Message struct {
 	Id   string `json:"id"`
 	Text string `json:"text"`
 }
+type MessageSource struct {
+	Type   string `json:"type"`
+	UserId string `json:"userId"`
+}
+
+type MessageEvent struct {
+	Type      string        `json:"type"`
+	Timestamp int64         `json:"timestamp"`
+	Source    MessageSource `json:"source"`
+	Message   Message       `json:"message"`
+}
+
+type Payload struct {
+	Destination string         `json:"destination"`
+	Events      []MessageEvent `json:"events"`
+}
 
 func (m *Message) String() string {
 	return fmt.Sprintf("Message{Type: %s, Id: %s, Text: %s}", m.Type, m.Id, m.Text)
