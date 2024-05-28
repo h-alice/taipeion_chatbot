40d18576ea09c3608f8b6c0051602820d455df6b
h-alice <admin@halice.art>
2024-05-28 11:41:02 +0800

    Add message test.

---
+++ taipeion/taipeion_message_test.go
@@ -0,0 +1,31 @@
+package taipeion
+
+import (
+	"testing"
+)
+
+func TestMessageSerializeWithOmittedField(t *testing.T) {
+
+	excepted := `{"ask":"broadcastMessage","message":{"type":"text","text":"Hello, World!"}}`
+	// Create a new message
+	message := Message{
+		Type: "text",
+		Text: "Hello, World!",
+	}
+	payload := ChannelMessagePayload{
+		Ask:     "broadcastMessage",
+		Message: message,
+	}
+
+	// Serialize the message
+	data, err := payload.Serialize()
+	if err != nil {
+		t.Errorf("Error serializing message: %v", err)
+	}
+	t.Log("Serialized message:", string(data))
+
+	if string(data) != excepted {
+		t.Errorf("Expected %s, got %s", excepted, string(data))
+	}
+
+}
