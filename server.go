21c26b7483cb7ae8a2eb83121c1aa0b1d4e0849d
h-alice <admin@halice.art>
2024-05-28 15:57:48 +0800

    Main server.

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -26,53 +27,9 @@ type ServerConfig struct {
 	Port               int16  `yaml:"port"`
 }
 
-func webhookIncomeRequestHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Check header for content length.
-	if r.Header.Get("Content-Length") == "" || r.Header.Get("Content-Length") == "0" {
-		// Ignore and send OK.
-		response := Response{Status: "no content"}
-		w.WriteHeader(http.StatusAccepted)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-		return
-	}
-
-	// Read the request body
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to read request body", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	// Print Header
-	log.Println("Header:", r.Header)
-
-	// Deserialize the message
-	payload, err := tp.DeserializeWebhookMessage(body)
-
-	if err != nil {
-		log.Println("Error deserializing message:", err)
-		// Fallback to printing the raw body
-		log.Println("Received payload:", string(body))
-		http.Error(w, "Malformed payload.", http.StatusBadRequest)
-		return
-	}
-
-	log.Printf("Received payload: %+v\n", payload)
-
-	// Create the response object
-	response := Response{Status: "success"}
-
-	// Set the response status to OK
-	w.WriteHeader(http.StatusOK)
-
-	// Set the response header to indicate JSON content
-	w.Header().Set("Content-Type", "application/json")
-
-	// Encode the response object to JSON and send it
-	json.NewEncoder(w).Encode(response)
+type InternalWebhookEvent struct {
+	Destination string
+	tp.MessageEvent
 }
 
 type TaipeionBot struct {
@@ -81,6 +38,11 @@ type TaipeionBot struct {
 	ChannelAccessToken string
 	ServerAddress      string
 	ServerPort         int16
+	eventQueue         chan InternalWebhookEvent
+}
+
+func (tpb *TaipeionBot) EnqueueWebhookIncomingEvent(event InternalWebhookEvent) {
+	tpb.eventQueue <- event
 }
 
 func (tpb *TaipeionBot) SendBroadcastMessage(message string) error {
@@ -151,13 +113,100 @@ func (tpb *TaipeionBot) DoEndpointPostRequest(endpoint string, data []byte) erro
 }
 
 func (tpb *TaipeionBot) webhookEventListener() error {
-	http.HandleFunc("/", webhookIncomeRequestHandler)
+
+	// We embed the handler function inside the function to access the bot instance.
+	webhookIncomeRequestHandler := func(w http.ResponseWriter, r *http.Request) {
+
+		// Check header for content length.
+		if r.Header.Get("Content-Length") == "" || r.Header.Get("Content-Length") == "0" {
+			// Ignore and send OK.
+			response := Response{Status: "no content"}
+			w.WriteHeader(http.StatusAccepted)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		// Read the request body
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		// Print Header
+		log.Println("Header:", r.Header)
+
+		// Deserialize the message
+		payload, err := tp.DeserializeWebhookMessage(body)
+
+		if err != nil {
+			log.Println("Error deserializing message:", err)
+			// Fallback to printing the raw body
+			log.Println("Received payload:", string(body))
+			http.Error(w, "Malformed payload.", http.StatusBadRequest)
+			return
+		}
+
+		// Iterate over the events.
+		for _, event := range payload.Events {
+			// Create an internal event
+			internal_event := InternalWebhookEvent{
+				Destination:  payload.Destination,
+				MessageEvent: event,
+			}
+
+			// Enqueue the event
+			tpb.EnqueueWebhookIncomingEvent(internal_event)
+		}
+
+		// Create the response object
+		response := Response{Status: "success"}
+
+		// Set the response status to OK
+		w.WriteHeader(http.StatusOK)
+
+		// Set the response header to indicate JSON content
+		w.Header().Set("Content-Type", "application/json")
+
+		// Encode the response object to JSON and send it
+		json.NewEncoder(w).Encode(response)
+	}
+
+	http.HandleFunc("/", webhookIncomeRequestHandler) // Set the default handler.
+
+	// Start the server.
 	full_server_address := fmt.Sprintf("%s:%d", tpb.ServerAddress, tpb.ServerPort)
 	log.Println("[EventListener] Starting server at ", full_server_address)
-	return http.ListenAndServe(full_server_address, nil)
+
+	return http.ListenAndServe(full_server_address, nil) // Serve until error.
+}
+
+func (tpb *TaipeionBot) EventProcessorLoop(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Context cancelled. Exiting event processor loop.")
+			return nil
+
+		case event := <-tpb.eventQueue:
+			// Process the event
+			// For current implementation, we just echo the message back.
+			// Print the message
+			log.Printf("Received event: %#v\n", event)
+		}
+
+	}
 }
 
 func (tpb *TaipeionBot) Start() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go tpb.EventProcessorLoop(ctx) // Start the event processor loop
+
+	tpb.eventQueue = make(chan InternalWebhookEvent, 100)
 	return tpb.webhookEventListener()
 }
 
