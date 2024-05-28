5fd27c1760414e1e63e7829a36d55d1ef61dd804
h-alice <admin@halice.art>
2024-05-28 11:41:20 +0800

    Basic server.

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -13,15 +14,29 @@ import (
 
 // Define a struct for the response
 type Response struct {
-	Test string `json:"test"`
+	Status string `json:"test"`
 }
 
 type ServerConfig struct {
-	Address string `yaml:"address"`
-	Port    string `yaml:"port"`
+	Endpoint           string `yaml:"taipeion-endpoint"`
+	ChannelSecret      string `yaml:"channel-secret"`
+	ChannelAccessToken string `yaml:"channel-access-token"`
+	Address            string `yaml:"address"`
+	Port               int16  `yaml:"port"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func webhookIncomeRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Check header for content length.
+	if r.Header.Get("Content-Length") == "" || r.Header.Get("Content-Length") == "0" {
+		// Ignore and send OK.
+		response := Response{Status: "no content"}
+		w.WriteHeader(http.StatusAccepted)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
 	// Read the request body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -33,24 +48,67 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Print Header
 	log.Println("Header:", r.Header)
 
-	// Print the request body to the console
-	payload, err := tp.DeserializeMessage(body)
+	// Deserialize the message
+	payload, err := tp.DeserializeWebhookMessage(body)
+
 	if err != nil {
 		log.Println("Error deserializing message:", err)
 		// Fallback to printing the raw body
 		log.Println("Received payload:", string(body))
-	} else {
-		log.Println("Received payload:", payload)
+		http.Error(w, "Malformed payload.", http.StatusBadRequest)
+		return
 	}
 
+	log.Printf("Received payload: %+v\n", payload)
+
 	// Create the response object
-	response := Response{Test: "Helloworld"}
+	response := Response{Status: "success"}
+
+	// Set the response status to OK
+	w.WriteHeader(http.StatusOK)
 
 	// Set the response header to indicate JSON content
 	w.Header().Set("Content-Type", "application/json")
 
 	// Encode the response object to JSON and send it
 	json.NewEncoder(w).Encode(response)
+}
+
+type TaipeionBot struct {
+	Endpoint           string
+	ChannelSecret      string
+	ChannelAccessToken string
+	ServerAddress      string
+	ServerPort         int16
+}
+
+func (tpb *TaipeionBot) SendBroadcastMessage(message string) error {
+	return nil
+}
+
+func (tpb *TaipeionBot) webhookEventListener() error {
+	http.HandleFunc("/", webhookIncomeRequestHandler)
+	full_server_address := fmt.Sprintf("%s:%d", tpb.ServerAddress, tpb.ServerPort)
+	log.Println("[EventListener] Starting server at ", full_server_address)
+	return http.ListenAndServe(full_server_address, nil)
+}
+
+func (tpb *TaipeionBot) Start() error {
+	return tpb.webhookEventListener()
+}
+
+func NewChatbotInstance(endpoint string, channelSecret string, channelAccessToken string, serverAddress string, serverPort int16) *TaipeionBot {
+	return &TaipeionBot{
+		Endpoint:           endpoint,
+		ChannelSecret:      channelSecret,
+		ChannelAccessToken: channelAccessToken,
+		ServerAddress:      serverAddress,
+		ServerPort:         serverPort,
+	}
+}
+
+func NewChatbotFromConfig(config ServerConfig) *TaipeionBot {
+	return NewChatbotInstance(config.Endpoint, config.ChannelSecret, config.ChannelAccessToken, config.Address, config.Port)
 }
 
 func main() {
@@ -67,14 +125,10 @@ func main() {
 		log.Fatal("Error parsing config file:", err)
 	}
 
-	// Set the handler for the root path
-	http.HandleFunc("/", handler)
+	// Create a new chatbot instance
+	bot := NewChatbotFromConfig(config)
 
-	// Start the HTTP server.
-	log.Println("Starting server on", config.Address+":"+config.Port)
-	err = http.ListenAndServe(config.Address+":"+config.Port, nil)
-	if err != nil {
-		log.Fatal("Error starting server:", err)
-	}
+	// Start the chatbot
+	err = bot.Start()
 
 }
