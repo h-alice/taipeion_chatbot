7c6d1c8b4aad79059c89779d1fd78290f5a6a1d5
h-alice <admin@halice.art>
2024-07-03 09:48:13 +0800

    Add llm callback.

--- llm_connector.go
+++ llm_connector.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -28,17 +29,49 @@ func NewLlmConnector(llmEndpoint string) *LlmConnector {
 	}
 }
 
-// NOTE: We use stdout printing only to handling error.
-func LlmRequestSender(prompt LlmUserQuery, llmEndpoint string) string {
+func (c *LlmConnector) LlmCallback(bot *TaipeionBot, event ChatbotWebhookEvent) error {
+
+	// Check if incoming event is text message.
+	if event.Message.Type != "text" {
+		log.Println("[PrivateMessageCallback] Received non-text message. Ignoring.")
+		return nil
+	}
+
+	chan_id := event.Destination
+	userId := event.Source.UserId
+	userQuery := event.Message.Text
+
+	// Create a new user query.
+	userQueryPayload := LlmUserQuery{
+		ChannelId: chan_id,
+		UserId:    userId,
+		Query:     userQuery,
+	}
+
+	// Send the user query to the LLM server.
+	response, err := c.LlmRequestSender(userQueryPayload)
+	if err != nil {
+		log.Println("[LlmCallback] Unable to send user query to LLM server:", err)
+		return err
+	}
+
+	finalResponse := fmt.Sprintf("%s\n%s", response.Response, response.Reference)
+
+	return bot.SendPrivateMessage(userId, finalResponse, chan_id)
+}
+
+func (c *LlmConnector) LlmRequestSender(prompt LlmUserQuery) (LlmModelResponse, error) {
 
 	request_payload, err := json.Marshal(prompt)
 	if err != nil {
 		log.Println("[LlmConnector] Unable to serialize user query:", err)
+		return LlmModelResponse{}, err
 	}
 
-	req, err := http.NewRequest("POST", llmEndpoint, bytes.NewBuffer(request_payload))
+	req, err := http.NewRequest("POST", c.LlmEndpoint, bytes.NewBuffer(request_payload))
 	if err != nil {
 		log.Println("[LlmConnector] Unable to create request:", err)
+		return LlmModelResponse{}, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -48,8 +81,17 @@ func LlmRequestSender(prompt LlmUserQuery, llmEndpoint string) string {
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("[LlmConnector] Unable to perform request:", err)
+		return LlmModelResponse{}, err
 	}
 	defer resp.Body.Close()
 
-	return ""
+	modelResp := LlmModelResponse{}
+
+	err = json.NewDecoder(resp.Body).Decode(&modelResp)
+	if err != nil {
+		log.Println("[LlmConnector] Unable to decode response:", err)
+		return LlmModelResponse{}, err
+	}
+
+	return modelResp, nil
 }
