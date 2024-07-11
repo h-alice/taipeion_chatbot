8917dabdf225ea93d1608eda208e1b779bb266af
h-alice <admin@halice.art>
2024-07-11 15:02:20 +0800

    Removed delayed logger.

--- main.go
+++ main.go
@@ -3,27 +3,9 @@ package main
 import (
 	"log"
 	"os"
-	"time"
 
 	"gopkg.in/yaml.v3"
 )
-
-func DelayedLoggingEventCallback(bot *TaipeionBot, event ChatbotWebhookEvent) error {
-	// Check if incoming event is text message.
-	if event.Message.Type != "text" {
-		log.Println("[DelayedLogger] Received non-text message. Ignoring.")
-		return nil
-	}
-
-	log.Printf("[DelayedLogger] >>>>>>>>>> Received event: %#v\n", event)
-
-	log.Println("[DelayedLogger] >>>>>>>>>> Entering DelayedLoggingEventCallback, sleeping for 5 seconds.")
-
-	time.Sleep(5 * time.Second)
-
-	log.Println("[DelayedLogger] <<<<<<<<<< Leaving DelayedLoggingEventCallback.")
-	return nil
-}
 
 func main() {
 	// Read the server configuration from file.
