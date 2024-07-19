8711ab7178228a2c9a88cc01fe2a73a1736ec15a
h-alice <admin@halice.art>
2024-07-19 16:16:17 +0800

    Rename symbol.

--- server_prototype.go
+++ server_prototype.go
@@ -22,17 +22,17 @@ type Channel struct {
 	ChannelLlmEndpoint string `yaml:"llm-endpoint"`
 }
 
-type ChannelIdMap map[int]Channel
+type ChannelIdConfigMap map[int]Channel
 
 type ServerConfig struct {
-	Endpoint               string       `yaml:"taipeion-endpoint"`
-	Channels               ChannelIdMap `yaml:"channels"`
-	Address                string       `yaml:"address"`
-	Port                   int16        `yaml:"port"`
-	ApiPlatformEndpoint    string       `yaml:"api-platform-endpoint"`
-	ApiPlatformClientId    string       `yaml:"api-platform-client-id"`
-	ApiPlatformClientToken string       `yaml:"api-platform-client-token"`
-	MaxConcurrentEvent     int          `yaml:"max-concurrent-event-handlers"`
+	Endpoint               string             `yaml:"taipeion-endpoint"`
+	Channels               ChannelIdConfigMap `yaml:"channels"`
+	Address                string             `yaml:"address"`
+	Port                   int16              `yaml:"port"`
+	ApiPlatformEndpoint    string             `yaml:"api-platform-endpoint"`
+	ApiPlatformClientId    string             `yaml:"api-platform-client-id"`
+	ApiPlatformClientToken string             `yaml:"api-platform-client-token"`
+	MaxConcurrentEvent     int                `yaml:"max-concurrent-event-handlers"`
 }
 
 type ChatbotWebhookEvent struct {
