118b0775615f56e4ecfed53a64928ab967eed77e
h-alice <admin@halice.art>
2024-07-24 14:31:22 +0800

    Add definition for new event handler structure.

--- server.go
+++ server.go
@@ -17,6 +17,11 @@ import (
 
 type WebhookEventCallback func(*TaipeionBot, ChatbotWebhookEvent) error
 
+type eventHandlerEntry struct {
+	callback   WebhookEventCallback
+	isPirority bool
+}
+
 // Define a struct for the response
 type response struct {
 	Status string `json:"status"`
