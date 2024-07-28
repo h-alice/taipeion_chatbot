9e8737897d056d392510f20ff3e0980816a594c9
h-alice <admin@halice.art>
2024-07-28 14:35:20 +0800

    Add priority task defiition.

--- server_task_priority.go
+++ server_task_priority.go
@@ -1,1 +1,15 @@
 package main
+
+func ScheduleNormalPriority(callback WebhookEventCallback) eventHandlerEntry {
+	return eventHandlerEntry{
+		callback:   callback,
+		isPriority: false,
+	}
+}
+
+func ScheduleHighestPriority(callback WebhookEventCallback) eventHandlerEntry {
+	return eventHandlerEntry{
+		callback:   callback,
+		isPriority: true,
+	}
+}
